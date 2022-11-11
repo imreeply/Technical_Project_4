724c885af79b8cf95474086f8eec2a1dbd74ab07
Reeply <110552455+imreeply@users.noreply.github.com>
2022-11-11 13:27:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavbarComponents from "./components/Navbar";
 import FooterComponent from "./components/Footer";
+import HomePage from "../pages/home";
 
 function App() {
   return (
     <>
       <NavbarComponents />
       <FooterComponent />
+      <HomePage />
     </>
   );
 }
